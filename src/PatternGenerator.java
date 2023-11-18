c11c4c612ee63b3c81e7918d28decce0e1d83390
Srrijan Arora <srijanarora@hotmail.com>
2023-11-18 20:48:33 +0530

    Base pattern generator

--- src/PatternGenerator.java
+++ src/PatternGenerator.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+//recursion based program, stack, queue, LL implement not applicable rn
 public class PatternGenerator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to Pattern Generator!");
-
+        //need to add more pattern types here
         System.out.print("Enter pattern type (square, triangle): ");
         String patternType = scanner.nextLine();
 
@@ -24,7 +25,7 @@ public class PatternGenerator {
             System.out.println("Invalid pattern type entered. Please choose square or triangle.");
         }
     }
-
+    //name methods only with "generate<pattern type>Pattern
     public static void generateSquarePattern(int size, char character) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
